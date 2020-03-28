7b65c4e3110cb54db2a6c2cd7e21acc3d78077fa
alineschneider <61927605+alineschneider@users.noreply.github.com>
2020-03-28 19:54:48 -0400

    Updating PyPoll Challenge, + csv and results.txt

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -3,7 +3,7 @@ import csv
 import os
 
 #Assign a variable for the file to load and the path
-file_to_load = os.path.join("Resources/election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
